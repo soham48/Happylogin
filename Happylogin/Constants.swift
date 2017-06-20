518dc8f5c216cd54c249e4fc96a1928548abd911
Soham Mozar <soham@Sohams-iMac.local>
2017-06-20 07:19:29 +0530

    sign in ui cutom complete

---
+++ Happylogin/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Happylogin
+//
+//  Created by Soham Mozar on 20/06/17.
+//  Copyright © 2017 abc. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 0
+
