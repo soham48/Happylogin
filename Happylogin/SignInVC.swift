6a227186333538fd716b6f200164fa078a3f181a
Soham Mozar <soham@Sohams-iMac.local>
2017-06-20 05:30:55 +0530

    Completed setting up FIREBASE setup

---
+++ Happylogin/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happylogin
+//
+//  Created by Soham Mozar on 20/06/17.
+//  Copyright © 2017 abc. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Happylogin/Pods-Happylogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Happylogin : NSObject
+@end
+@implementation PodsDummy_Pods_Happylogin
+@end
